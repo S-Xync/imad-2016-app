ae2f16eb93f3b20102633291a8766bf1f73892a4
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-26 19:10:08 +0530

    Captures response from url end point as names string

--- ui/main.js
+++ ui/main.js
@@ -19,12 +19,22 @@ var name=nameInput.value;
 var submit=document.querySelector("#submit-btn");
 submit.onclick=function(){
   //make a request
-  //capture the response
-  var names=["name1","name2","name3","name4"];
-  var list='';
-  for(var i=0;i<names.length;i++){
-    list+='<li>'+names[i]+'</li>';
-  }
-  var ul=document.querySelector("#namelist");
-  ul.innerHTML=list;
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if (request.readyState===XMLHttpRequest.DONE){
+      if (request.status===200) {
+        //capture the response
+        var names=request.responseText;
+        names=JSON.parse(names);
+        var list='';
+        for(var i=0;i<names.length;i++){
+          list+='<li>'+names[i]+'</li>';
+        }
+        var ul=document.querySelector("#namelist");
+        ul.innerHTML=list;
+      }
+    }
+  };
+  request.open('GET','http://localhost:8080/submit-name?name='+name,true)
+  request.send(null);
 };
