e1973d91cf1874a7b217be6d9b7fc99ca8a8530b
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-09-26 18:58:34 +0530

    Names submits using querystring

--- server.js
+++ server.js
@@ -125,20 +125,19 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js',function(req,res){
 	res.sendFile(path.join(__dirname,'ui','main.js'));
 });
-app.get('/:articleName',function(req,res){
-	var articleName=req.params.articleName;
-	res.send(createtemplate(articles[articleName]));
+names=[];
+app.get('/submit-name/',function(req,res){//URL : "/submit-name?name=name1"
+var name=req.query.name;
+names.push(name);
+res.send(JSON.stringify(names));
 });
 app.get('/ui/madi.png', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-names=[];
-app.get('/submit-name/:name',function(req,res){
-	var name=req.params.name;
-	names.push(name);
-	res.send(JSON.stringify(names));
+app.get('/:articleName',function(req,res){
+	var articleName=req.params.articleName;
+	res.send(createtemplate(articles[articleName]));
 });
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
 	console.log(`App listening on port ${port}!`);
