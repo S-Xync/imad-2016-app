297c60c0a50bb805399a9a2fad2d4757c702ae2a
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-10-25 16:23:09 +0530

    Implements a connection in database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+	user:'postgres',
+	database:'mydb1',
+	host:'127.0.0.1',
+	port:'5432',
+	password:'postgres'
+};
 var app = express();
 app.use(morgan('combined'));
 var articleName;
@@ -118,7 +125,16 @@ app.get('/counter',function(req,res){
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+	pool.query('SELECT * FROM test',function(err,result){
+		if (err) {
+			res.status(500).send(err.toString());
+		}else {
+			res.send(JSON.stringify(result));
+		}
+	});
+});
 app.get('/ui/style.css', function (req, res) {
 	res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
