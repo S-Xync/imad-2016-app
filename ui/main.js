c49e55d112a6f292afbbfbe2e40e9ce6c98b7fd1
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-11-18 15:28:14 +0530

    Post request added to main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,23 @@
-//code for the counter
-var counterButton=document.querySelector("#counter");
-counterButton.onclick=function(){
-  var request=new XMLHttpRequest();
-  request.onreadystatechange=function(){
-    if (request.readyState===XMLHttpRequest.DONE){
-      if (request.status===200) {
-        var counter=request.responseText;
-        var count=document.querySelector("#count");
-        count.innerHTML=counter.toString();
-      }
-    }
-  };
-  request.open('GET','http://localhost:8080/counter',true)
-  request.send(null);
-};
 var submit=document.querySelector("#submit-btn");
 submit.onclick=function(){
-  var nameInput=document.querySelector("#name");
-  var name=nameInput.value;
   //make a request
   var request=new XMLHttpRequest();
   request.onreadystatechange=function(){
     if (request.readyState===XMLHttpRequest.DONE){
       if (request.status===200) {
-        //capture the response
-        var names=request.responseText;
-        names=JSON.parse(names);
-        var list='';
-        for(var i=0;i<names.length;i++){
-          list+='<li>'+names[i]+'</li>';
-        }
-        var ul=document.querySelector("#namelist");
-        ul.innerHTML=list;
+        alert("Successfully Logged In");
+      }else if(request.status===403){
+        alert("Username/Password incorrect");
+      }else if(request.status===500){
+        alert("Something Went Wrong");
       }
     }
   };
-  request.open('GET','http://localhost:8080/submit-name?name='+name,true)
-  request.send(null);
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://localhost:8080/login',true);
+  request.setRequestHeader('Content-Type-application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 };
